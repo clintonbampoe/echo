009e2fdb4d35b369aea489bb011eba9bdc9b62fc
clint@debian <clintbamp@gmail.com>
2026-05-16 14:03:32 +0000

    feat(database): implement app dbcontext and wire up postgres connection

---
+++ src/Backend.Api.Core/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Api.Core.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+    }
+}
--- src/Backend.Api.Core/Program.cs
+++ src/Backend.Api.Core/Program.cs
@@ -1,9 +1,14 @@
+using Backend.Api.Core.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+var template = GetConnectionStringTemplate();
+var (username, password) = GetLocalDatabaseCredentials();
+var connectionString = BuildConnectionStringFromTemplate(template);
 
+builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(connectionString));
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
@@ -21,3 +26,18 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+string? GetConnectionStringTemplate() =>
+    builder.Configuration.GetConnectionString("DefaultConnection");
+
+(string? username, string? password) GetLocalDatabaseCredentials()
+{
+    var dbUsername = builder.Configuration["Database:Username"];
+    var dbPassword = builder.Configuration["Database:Password"];
+    return (dbUsername, dbPassword);
+}
+
+string BuildConnectionStringFromTemplate(string? stringTemplate)
+{
+    return string.Format(stringTemplate!, username, password);
+}
