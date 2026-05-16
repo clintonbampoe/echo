ab6b70b3e565abbb51d7185695850187823786b8
clint@debian <clintbamp@gmail.com>
2026-05-16 23:34:31 +0000

    refactor(entity): make non-queried entity fields nullable

--- src/Backend.Api.Core/Entities/Event.cs
+++ src/Backend.Api.Core/Entities/Event.cs
@@ -8,11 +8,11 @@ public class Event
     public string Title { get; init; } = string.Empty;
     public DateOnly StartDate { get; init; }
     public DateOnly EndDate { get; init; }
-    public TimeOnly StartTime { get; init; }
-    public TimeOnly EndTime { get; init; }
-    public string Location { get; init; } = string.Empty;
+    public TimeOnly? StartTime { get; init; }
+    public TimeOnly? EndTime { get; init; }
+    public string? Location { get; init; } = string.Empty;
     public int OrganizationId { get; init; }
     public int OrganizerId { get; init; }
-    public int Capacity { get; init; }
-    public string Description { get; init; } = string.Empty;
+    public int? Capacity { get; init; }
+    public string? Description { get; init; } = string.Empty;
 }
--- src/Backend.Api.Core/Entities/Member.cs
+++ src/Backend.Api.Core/Entities/Member.cs
@@ -9,17 +9,17 @@ public class Member
     public Guid UniqueId { get; init; }
     public string FirstName { get; init; } = string.Empty;
     public string LastName { get; init; } = string.Empty;
-    public string OtherNames { get; init; } = string.Empty;
-    public string EmailAddress { get; init; } = string.Empty;
+    public string? OtherNames { get; init; } = string.Empty;
+    public string? EmailAddress { get; init; } = string.Empty;
     public string PhoneNumber { get; init; } = string.Empty;
     public DateOnly DateOfBirth { get; init; }
-    public DateOnly JoinedDate { get; init; }
+    public DateOnly? JoinedDate { get; init; }
     public Gender Gender { get; init; }
     public string ResidentialAddress { get; init; } = string.Empty;
     public string City { get; init; } = string.Empty;
     public string Hometown { get; init; } = string.Empty;
     public Region Region { get; init; }
-    public string GpsAddress { get; init; } = string.Empty;
+    public string? GpsAddress { get; init; } = string.Empty;
     public MaritalStatus MaritalStatus { get; init; }
     public string NextOfKin { get; init; } = string.Empty;
     public string EmergencyContactName { get; init; } = string.Empty;
