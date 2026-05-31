81930e32fc323c579731ec558c4cd7d1cffe8977
Clinton Bampoe <clintbamp@gmail.com>
2026-05-31 05:13:29 -0500

    build(database): run migrations to implement soft delete logic

---
+++ src/Backend.Api.Core/Migrations/20260531101058_AddFieldForSoftDeleteImplementation.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Backend.Api.Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddFieldForSoftDeleteImplementation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletedAt",
+                table: "Users",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletedAt",
+                table: "Transactions",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletedAt",
+                table: "TransactionCategories",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletedAt",
+                table: "Tithes",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletedAt",
+                table: "Projects",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletedAt",
+                table: "ProjectContributions",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletedAt",
+                table: "ProjectCategories",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletedAt",
+                table: "Organizations",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletedAt",
+                table: "OrganizationMembers",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletedAt",
+                table: "Members",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletedAt",
+                table: "Events",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletedAt",
+                table: "EventRegistrations",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletedAt",
+                table: "EventAttendances",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletedAt",
+                table: "Congregations",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletedAt",
+                table: "AttendanceRecords",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletedAt",
+                table: "Assets",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletedAt",
+                table: "AssetCategories",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AssetCategories_CongregationId",
+                table: "AssetCategories",
+                column: "CongregationId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AssetCategories_Congregations_CongregationId",
+                table: "AssetCategories",
+                column: "CongregationId",
+                principalTable: "Congregations",
+                principalColumn: "CongregationId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AssetCategories_Congregations_CongregationId",
+                table: "AssetCategories");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AssetCategories_CongregationId",
+                table: "AssetCategories");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedAt",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedAt",
+                table: "Transactions");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedAt",
+                table: "TransactionCategories");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedAt",
+                table: "Tithes");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedAt",
+                table: "Projects");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedAt",
+                table: "ProjectContributions");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedAt",
+                table: "ProjectCategories");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedAt",
+                table: "Organizations");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedAt",
+                table: "OrganizationMembers");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedAt",
+                table: "Members");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedAt",
+                table: "Events");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedAt",
+                table: "EventRegistrations");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedAt",
+                table: "EventAttendances");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedAt",
+                table: "Congregations");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedAt",
+                table: "AttendanceRecords");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedAt",
+                table: "Assets");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedAt",
+                table: "AssetCategories");
+        }
+    }
+}
