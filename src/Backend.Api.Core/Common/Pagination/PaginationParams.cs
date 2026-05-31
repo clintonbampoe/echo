e2ccffc05b33213cea8902ed20e1120df5df6559
Clinton Bampoe <clintbamp@gmail.com>
2026-05-31 18:47:50 -0500

    refactor(pagination): enhance PageSize setter for improved validation

--- src/Backend.Api.Core/Common/Pagination/PaginationParams.cs
+++ src/Backend.Api.Core/Common/Pagination/PaginationParams.cs
@@ -1,12 +1,21 @@
 public class PaginationParams
 {
     private const int MaxPageSize = 50;
+    private int _pageSize = 25;
     public int PageNumber { get; set; } = 1;
 
-    private int _pageSize = 10;
-    private int PageSize
+    public int PageSize
     {
         get => _pageSize;
-        set => _pageSize = (value > MaxPageSize) ? MaxPageSize : value;
+        set
+        {
+            if (value < 1)
+                _pageSize = 1;
+            else if (value > MaxPageSize)
+                _pageSize = MaxPageSize;
+            else
+                _pageSize = value;
+
+        }
     }
 }
