abad359a5cad1343bd4e6d2d604f6562bb8c27be
Clinton Bampoe <clintbamp@gmail.com>
2026-06-01 13:54:09 -0500

    refactor(extensions): separate pagination and search filter methods into separate classes

---
+++ src/Backend.Api.Core/Common/ExtensionMethods/PaginationFilterMethods.cs
@@ -0,0 +1,48 @@
+using System.Linq.Expressions;
+
+namespace Backend.Api.Core.Common.ExtensionMethods;
+
+public static class PaginationFilterMethods
+{
+    public static IQueryable<T> ApplyPagination<T>(this IQueryable<T> query, PaginationParams paginationParams)
+        where T : class, ICongregationEntity
+    {
+        if (paginationParams is null)
+            return query;
+
+        return query
+            .OrderByGuidIfUnordered()
+            .Skip((paginationParams.PageNumber - 1) * paginationParams.PageSize)
+            .Take(paginationParams.PageSize);
+    }
+
+    private static IQueryable<T> OrderByGuidIfUnordered<T>(this IQueryable<T> query)
+        where T : class, ICongregationEntity
+    {
+        if (IsOrderedQuery(query))
+            return query;
+
+        return query.OrderBy(e => e.Id);
+    }
+
+    private static bool IsOrderedQuery<T>(IQueryable<T> query)
+    {
+        return HasOrderBy(query.Expression);
+    }
+
+    private static bool HasOrderBy(Expression expression)
+    {
+        if (expression is MethodCallExpression call)
+        {
+            string name = call.Method.Name;
+            if (call.Method.DeclaringType == typeof(Queryable) &&
+                (name == "OrderBy" || name == "OrderByDescending" ||
+                name == "ThenBy" || name == "ThenByDescending"))
+            {
+                return true;
+            }
+            return HasOrderBy(call.Arguments[0]);
+        }
+        return false;
+    }
+}
