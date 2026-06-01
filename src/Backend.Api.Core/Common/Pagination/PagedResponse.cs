4e26a97a66692ff74f1e06c8ccb61a2e094dea20
Clinton Bampoe <clintbamp@gmail.com>
2026-06-01 17:13:11 -0500

    refactor(pagination): update PagedResponse constructor to use PaginationParams for better clarity

--- src/Backend.Api.Core/Common/Pagination/PagedResponse.cs
+++ src/Backend.Api.Core/Common/Pagination/PagedResponse.cs
@@ -5,11 +5,11 @@ public class PagedResponse<T> where T : class
     public int PageSize { get; set; }
     public int TotalRecords { get; set; }
 
-    public PagedResponse(List<T> data, int pageNumber, int pageSize, int totalRecords)
+    public PagedResponse(List<T> data, PaginationParams paginationParams, int totalRecords)
     {
         Data = data;
-        PageNumber = pageNumber;
-        PageSize = pageSize;
+        PageNumber = paginationParams.PageNumber;
+        PageSize = paginationParams.PageSize;
         TotalRecords = totalRecords;
     }
 }
