ca7aea0b38d2f711871804b3ce8cb3d7a3c53660
Clinton Bampoe <clintbamp@gmail.com>
2026-06-01 12:48:23 -0500

    feat(repositories): consolidate member repository CRUD  services under IDomainRecordService

--- src/Backend.Api.Core/Repositories/MemberRepository.cs
+++ src/Backend.Api.Core/Repositories/MemberRepository.cs
@@ -13,31 +13,24 @@ public class MemberRepository : IEntityRepository<Member>
     private readonly DbContext _context;
     private readonly DbSet<Member> _dbSet;
     private readonly IMapper _mapper;
-    private readonly ISoftDeleteService<Member> _softDeleteService;
-    private readonly IUpdateRecordService<Member> _updateRecordService;
-    private readonly IGetRecordService<Member> _getRecordService;
-    private readonly ICreateNewRecordService<Member> _createNewRecordService;
+    private readonly IDomainRecordService<Member> _domainRecordService;
+
     public MemberRepository(
         AppDbContext context,
         IMapper mapper,
-        ISoftDeleteService<Member> softDeleteService,
-        IUpdateRecordService<Member> updateRecordService,
-        IGetRecordService<Member> getRecordService,
-        ICreateNewRecordService<Member> createNewRecordService)
+        IDomainRecordService<Member> domainRecordService
+        )
     {
         _context = context;
         _dbSet = context.Set<Member>();
-        _softDeleteService = softDeleteService;
         _mapper = mapper;
-        _updateRecordService = updateRecordService;
-        _getRecordService = getRecordService;
-        _createNewRecordService = createNewRecordService;
+        _domainRecordService = domainRecordService;
     }
 
     public async Task<PagedResponse<Member>> GetPageAsync(
         PaginationParams paginationParameters, QueryParameters queryParameters, CancellationToken cancellationToken = default)
     {
-        var totalRecords = _getRecordService.GetTotalRecordsCount(queryParameters);
+        var totalRecords = _domainRecordService.GetTotalRecordsCount(queryParameters);
 
         var records = await _dbSet
             .AsNoTracking()
@@ -45,18 +38,18 @@ public class MemberRepository : IEntityRepository<Member>
             .ApplyPagination(paginationParameters)
             .ToListAsync(cancellationToken);
 
-        return _getRecordService.CreateNewPagedResponseObject(records, paginationParameters, totalRecords);
+        return _domainRecordService.CreateNewPagedResponseObject(records, paginationParameters, totalRecords);
     }
 
     public async Task<Member?> GetByIdAsync(Guid Id, CancellationToken cancellationToken = default)
     {
-        return await _getRecordService.GetRecordByIdAsync(Id, cancellationToken);
+        return await _domainRecordService.GetRecordByIdAsync(Id, cancellationToken);
     }
 
     public async Task<bool> CreateRecord(Member newRecordData, CancellationToken cancellationToken = default)
     {
         var recordCreatedSuccessfully =
-            await _createNewRecordService.CreateNewRecord(newRecordData, cancellationToken);
+            await _domainRecordService.CreateNewRecord(newRecordData, cancellationToken);
 
         if (!recordCreatedSuccessfully)
             return false;
@@ -69,7 +62,7 @@ public class MemberRepository : IEntityRepository<Member>
         Guid Id, Member updatedRecordData, CancellationToken cancellationToken = default)
     {
         var recordUpdatedSuccessfully =
-            await _updateRecordService.UpdateRecord(Id, updatedRecordData, cancellationToken);
+            await _domainRecordService.UpdateRecordById(Id, updatedRecordData, cancellationToken);
 
         if (!recordUpdatedSuccessfully)
             return false;
@@ -80,7 +73,7 @@ public class MemberRepository : IEntityRepository<Member>
 
     public async Task<bool> DeleteRecord(Guid Id, CancellationToken cancellationToken = default)
     {
-        var recordDeletedSuccessfully = await _softDeleteService.SoftDeleteByIdAsync(Id, cancellationToken);
+        var recordDeletedSuccessfully = await _domainRecordService.SoftDeleteByIdAsync(Id, cancellationToken);
 
         if (!recordDeletedSuccessfully)
             return false;
