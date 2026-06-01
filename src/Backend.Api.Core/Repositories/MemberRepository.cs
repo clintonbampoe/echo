91a7641997226d8e5930d1b428243b587d1b4478
Clinton Bampoe <clintbamp@gmail.com>
2026-06-01 07:33:43 -0500

    feat(repository): Enhance MemberRepository with Create, Update, and SoftDelete methods

--- src/Backend.Api.Core/Repositories/MemberRepository.cs
+++ src/Backend.Api.Core/Repositories/MemberRepository.cs
@@ -4,49 +4,47 @@ using Backend.Api.Core.Entities;
 using Backend.Api.Core.Entities.Dtos;
 using Backend.Api.Core.Entities.Dtos.Interfaces;
 using Backend.Api.Core.Repositories.Interfaces;
+using Backend.Api.Core.Services.Interfaces;
 using Microsoft.EntityFrameworkCore;
 
 namespace Backend.Api.Core.Repositories;
 
 public class MemberRepository : IEntityRepository<Member>
 {
-    private readonly AppDbContext _context;
+    private readonly DbContext _context;
     private readonly DbSet<Member> _dbSet;
-    public MemberRepository(AppDbContext context)
+    private readonly ISoftDeleteService<Member> _softDeleteService;
+    public MemberRepository(AppDbContext context, ISoftDeleteService<Member> softDeleteService)
     {
         _context = context;
         _dbSet = context.Set<Member>();
+        _softDeleteService = softDeleteService;
     }
 
     public async Task<PagedResponse<IResponseDto<Member>>> GetPageAsync(
-        PaginationParams paginationParameters,
-        QueryParameters queryParameters,
-        CancellationToken cancellationToken = default)
+        PaginationParams paginationParameters, QueryParameters queryParameters, CancellationToken cancellationToken = default)
     {
         var totalRecords = await _dbSet
             .AsNoTracking()
             .ApplyFilters(queryParameters)
             .CountAsync(cancellationToken);
 
-        var response = await _dbSet
+        var records = await _dbSet
             .AsNoTracking()
             .ApplyFilters(queryParameters)
             .ApplyPagination(paginationParameters)
-            .Select(m => (IResponseDto<Member>)new MemberResponseDto
+            .Select(m => new MemberListResponseDto
             {
                 Id = m.Id,
                 CongregationId = m.CongregationId,
-                FirstName = m.FirstName,
-                LastName = m.LastName,
-                OtherNames = m.OtherNames,
-                DateOfBirth = m.DateOfBirth,
-                Email = m.EmailAddress,
-                PhoneNumber = m.PhoneNumber
+                FullName = $"{m.FirstName} {m.LastName} {m.OtherNames}"
             })
             .ToListAsync(cancellationToken);
 
+        var responseAsDtos = records.Cast<IResponseDto<Member>>().ToList();
+
         return new PagedResponse<IResponseDto<Member>>(
-            response, paginationParameters.PageNumber, paginationParameters.PageSize, totalRecords);
+            responseAsDtos, paginationParameters.PageNumber, paginationParameters.PageSize, totalRecords);
     }
 
     public async Task<IResponseDto<Member>?> GetByIdAsync(Guid Id, CancellationToken cancellationToken = default)
@@ -68,18 +66,35 @@ public class MemberRepository : IEntityRepository<Member>
             .FirstOrDefaultAsync(cancellationToken);
     }
 
-    public Task<bool> CreateRecord(ICreateDto<Member> recordData, CancellationToken cancellationToken = default)
+    public async Task<bool> CreateRecord(ICreateDto<Member> recordData, CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException();
+        await _dbSet.AddAsync(new Member
+        {
+            Id = recordData.Id,
+            CongregationId = recordData.CongregationId
+        }, cancellationToken);
+        await _context.SaveChangesAsync(cancellationToken);
+        return true;
     }
 
-    public Task<bool> UpdateRecord(Guid Id, IUpdateDto<Member> recordData, CancellationToken cancellationToken = default)
+    public async Task<bool> UpdateRecord(
+        Guid Id, IUpdateDto<Member> recordData, CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException();
+        if (recordData is null)
+            return false;
+
+        var projectAsConcreteObject = (MemberUpdateDto)recordData;
+
+        await _dbSet.Where(m => m.Id == Id).ExecuteUpdateAsync(
+            s => s.SetProperty(m => m.FirstName, projectAsConcreteObject.FirstName)
+                .SetProperty(m => m.LastName, projectAsConcreteObject.LastName),
+            cancellationToken);
+
+        return true;
     }
 
-    public Task DeleteRecord(Guid Id, CancellationToken cancellationToken = default)
+    public async Task DeleteRecord(Guid Id, CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException();
+        await _softDeleteService.SoftDeleteByIdAsync(Id, cancellationToken);
     }
 }
