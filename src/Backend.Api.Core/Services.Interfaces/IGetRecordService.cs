520eb30b7b3a0222ae711c7fb0c8005e0ad3c93c
Clinton Bampoe <clintbamp@gmail.com>
2026-06-01 17:14:28 -0500

    refactor(services): remove GetTotalRecordsCount method from IGetRecordService interface

--- src/Backend.Api.Core/Services.Interfaces/IGetRecordService.cs
+++ src/Backend.Api.Core/Services.Interfaces/IGetRecordService.cs
@@ -4,8 +4,6 @@ public interface IGetRecordService<T> where T : class, ICongregationEntity
 {
     Task<T?> GetRecordByIdAsync(Guid Id, CancellationToken cancellationToken = default);
 
-    int GetTotalRecordsCount(QueryParameters queryParameters);
-
     PagedResponse<T> CreateNewPagedResponseObject(
         List<T> records, PaginationParams paginationParameters, int totalRecords);
 
