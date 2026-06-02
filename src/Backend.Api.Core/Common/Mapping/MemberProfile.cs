f72a83dcd52ce7c2cd085e90ab6514a28baf4d78
Clinton Bampoe <clintbamp@gmail.com>
2026-06-02 18:57:23 -0500

    feat(mapping): implement mapping profiles for OrganizationMember and Project entities

--- src/Backend.Api.Core/Common/Mapping/MemberProfile.cs
+++ src/Backend.Api.Core/Common/Mapping/MemberProfile.cs
@@ -8,11 +8,9 @@ public class MemberProfile : Profile
 {
     public MemberProfile()
     {
-        CreateMap<Member, MemberResponseDto>().ReverseMap();
-        CreateMap<Member, MemberCreateDto>().ReverseMap();
-        CreateMap<Member, MemberUpdateDto>().ReverseMap();
-        CreateMap<Member, MemberDeleteDto>().ReverseMap();
 
+        CreateMap<Member, MemberResponseDto>().ReverseMap();
+        
         CreateMap<Member, MemberListResponseDto>()
             .ForMember(dest => dest.Name, opt => opt.MapFrom(src =>
                 $"{src.FirstName} {src.LastName} {src.OtherNames}"))
@@ -21,5 +19,13 @@ public class MemberProfile : Profile
             .ForMember(dest => dest.FirstName, opt => opt.Ignore())
             .ForMember(dest => dest.LastName, opt => opt.Ignore())
             .ForMember(dest => dest.OtherNames, opt => opt.Ignore());
+
+        CreateMap<Member, MemberCreateDto>()
+            .ReverseMap()
+            .ForMember(dest => dest.DeletedAt, opt => opt.Ignore())
+            .ForMember(dest => dest.CreatedAt, opt => opt.MapFrom(src => DateTime.UtcNow));
+
+        CreateMap<Member, MemberUpdateDto>().ReverseMap();
+        CreateMap<Member, MemberDeleteDto>().ReverseMap();
     }
 }
