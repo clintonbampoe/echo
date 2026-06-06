178dfdbaa137365cee83dc44980ada561d5c25ef
Clinton Bampoe <clintbamp@gmail.com>
2026-06-06 17:24:15 -0500

    feat(common): reinforced query filter methods against null objects

--- src/Backend.Api.Core/Common/ExtensionMethods/QueryFilterMethods.cs
+++ src/Backend.Api.Core/Common/ExtensionMethods/QueryFilterMethods.cs
@@ -4,18 +4,24 @@ namespace Backend.Api.Core.Common.ExtensionMethods;
 
 public static class QueryFilterMethods
 {
-    public static IQueryable<T> ApplySearchFilter<T>(this IQueryable<T> query, QueryParameters queryParameters)
+    public static IQueryable<T> ApplySearchFilter<T>(this IQueryable<T> query, QueryParameters? queryParameters)
         where T : class, ISearchableEntity
     {
+        if (queryParameters is null)
+            return query;
+            
         if (string.IsNullOrWhiteSpace(queryParameters.SearchTerm))
             return query;
 
         return query.Where(x => x.Name.Contains(queryParameters.SearchTerm));
     }
 
-    public static IQueryable<T> ApplyDateFilters<T>(this IQueryable<T> query, QueryParameters queryParameters)
+    public static IQueryable<T> ApplyDateFilters<T>(this IQueryable<T> query, QueryParameters? queryParameters)
         where T : class, IDateTrackedEntity
     {
+        if (queryParameters is null)
+            return query;
+
         if (queryParameters.StartDate.HasValue)
             query = query.Where(x => x.CreatedAt >= queryParameters.StartDate.Value);
 
