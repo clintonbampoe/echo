2843d004d7793fa64966ff68c2d10b65a3423f82
Clinton Bampoe <clintbamp@gmail.com>
2026-06-06 17:25:35 -0500

    refactor: change constructor from base db context to 'AppDbContext'

--- src/Backend.Api.Core/Repositories/Base/EntityRepositoryBase.cs
+++ src/Backend.Api.Core/Repositories/Base/EntityRepositoryBase.cs
@@ -1,5 +1,6 @@
 using AutoMapper;
 using Backend.Api.Core.Common.ExtensionMethods;
+using Backend.Api.Core.Data;
 using Backend.Api.Core.Entities.Interfaces;
 using Backend.Api.Core.Repositories.Engines.Interfaces;
 using Microsoft.EntityFrameworkCore;
@@ -8,13 +9,13 @@ namespace Backend.Api.Core.Repositories.Base;
 
 public abstract class EntityRepositoryBase<T> where T : class, ICongregationEntity, ISoftDeletableEntity
 {
-    protected readonly DbContext _context;
+    protected readonly AppDbContext _context;
     protected readonly DbSet<T> _dbSet;
     protected readonly IMapper _mapper;
     protected readonly IDatabaseEngine<T> _databaseEngine;
 
     public EntityRepositoryBase(
-        DbContext context,
+        AppDbContext context,
         IMapper mapper,
         IDatabaseEngine<T> domainRecordService
         )
--- src/Backend.Api.Core/Repositories/Base/RelationshipRepositoryBase.cs
+++ src/Backend.Api.Core/Repositories/Base/RelationshipRepositoryBase.cs
@@ -1,6 +1,7 @@
 using System.Linq.Expressions;
 using AutoMapper;
 using Backend.Api.Core.Common.ExtensionMethods;
+using Backend.Api.Core.Data;
 using Backend.Api.Core.Entities.Interfaces;
 using Backend.Api.Core.Repositories.Engines.Interfaces;
 using Microsoft.EntityFrameworkCore;
@@ -10,7 +11,7 @@ namespace Backend.Api.Core.Repositories.Base;
 public abstract class RelationshipRepositoryBase<T> : EntityRepositoryBase<T>
     where T : class, ICongregationEntity, ISoftDeletableEntity
 {
-    protected RelationshipRepositoryBase(DbContext context, IMapper mapper, IDatabaseEngine<T> databaseEngine)
+    protected RelationshipRepositoryBase(AppDbContext context, IMapper mapper, IDatabaseEngine<T> databaseEngine)
         : base(context, mapper, databaseEngine)
     {
     }
