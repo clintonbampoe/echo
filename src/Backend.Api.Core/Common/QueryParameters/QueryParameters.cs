8d31f46bd2f00ef5fd0ea9f732276942ba220f67
Clinton Bampoe <clintbamp@gmail.com>
2026-06-06 15:59:26 -0500

    feat(common): change QueryParameters from abstract to concrete class

--- src/Backend.Api.Core/Common/QueryParameters/QueryParameters.cs
+++ src/Backend.Api.Core/Common/QueryParameters/QueryParameters.cs
@@ -1,5 +1,9 @@
-public abstract class QueryParameters
+public class QueryParameters
 {
+    public QueryParameters()
+    {
+
+    }
     public string? SearchTerm { get; set; }
     public DateTime? StartDate { get; set; }
     public DateTime? EndDate { get; set; }
