e9914a51bc5d24b12915ed196895bb3f0ce4012e
Clinton Bampoe <clintbamp@gmail.com>
2026-06-06 16:20:05 -0500

    feat(httpresults): leave implementation of CreatedAtResult class for later

--- src/Backend.Api.Core/Common/HttpResults/CreatedAtResult.cs
+++ src/Backend.Api.Core/Common/HttpResults/CreatedAtResult.cs
@@ -1,6 +1,13 @@
 using Backend.Api.Core.Common.HttpResults.Interfaces;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Backend.Api.Core.Common.HttpResults;
 
-
+public class CreatedAtResult() : IOperationResult
+{
+    public ActionResult ToActionResult()
+    {
+        throw new NotImplementedException();
+    }
+}
