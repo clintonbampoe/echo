af0d938f00ee334a926a7df8f4bd5cee527ecccd
Clinton Bampoe <clintbamp@gmail.com>
2026-06-06 19:10:37 -0500

    fix(services): change constructor to correct repository type

--- src/Backend.Api.Core/Services/TransactionCategoryService.cs
+++ src/Backend.Api.Core/Services/TransactionCategoryService.cs
@@ -1,13 +1,13 @@
 using AutoMapper;
 using Backend.Api.Core.Entities;
-using Backend.Api.Core.Repositories.Base;
+using Backend.Api.Core.Repositories;
 using Backend.Api.Core.Services.Base;
 
 namespace Backend.Api.Core.Services;
 
 public class TransactionCategoryService : ServiceBase<TransactionCategory>
 {
-    public TransactionCategoryService(RepositoryBase<TransactionCategory> repository, IMapper mapper) : base(repository, mapper)
+    public TransactionCategoryService(TransactionCategoryRepository repository, IMapper mapper) : base(repository, mapper)
     {
     }
 }
