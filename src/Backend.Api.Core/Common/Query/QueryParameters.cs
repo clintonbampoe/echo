16bed2e4d191d3767587857f198113ab602051ab
Clinton Bampoe <clintbamp@gmail.com>
2026-06-07 20:19:07 -0500

    refactor(common): rename QueryParameters namespace for clarity

--- src/Backend.Api.Core/Common/Query/QueryParameters.cs
+++ src/Backend.Api.Core/Common/Query/QueryParameters.cs
@@ -1,9 +1,9 @@
+namespace Backend.Api.Core.Common.Query;
+
 public class QueryParameters
 {
-    public QueryParameters()
-    {
+    public QueryParameters() { }
 
-    }
     public string? SearchTerm { get; set; }
     public DateTime? StartDate { get; set; }
     public DateTime? EndDate { get; set; }
