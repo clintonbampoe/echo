c149563bf94c7ea7a81c952dd10121e1bb7e0332
Clinton Bampoe <clintbamp@gmail.com>
2026-06-08 19:08:20 -0500

    build(database): update database schema and add indexes
    - not done with the indexes so I shall return to complete them

---
+++ src/Backend.Api.Core/Migrations/20260609000620_MinorCorrectionsToSchema.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Backend.Api.Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class MinorCorrectionsToSchema : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_AttendanceRecords_MemberId",
+                table: "AttendanceRecords");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Events_Name",
+                table: "Events",
+                column: "Name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AttendanceRecords_MemberId",
+                table: "AttendanceRecords",
+                column: "MemberId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Events_Name",
+                table: "Events");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AttendanceRecords_MemberId",
+                table: "AttendanceRecords");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AttendanceRecords_MemberId",
+                table: "AttendanceRecords",
+                column: "MemberId");
+        }
+    }
+}
