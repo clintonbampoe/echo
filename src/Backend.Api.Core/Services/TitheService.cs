4a49fba737aa2b3a711376de93aa0294408bbbf7
Clinton Bampoe <clintbamp@gmail.com>
2026-06-09 13:19:00 -0500

    feat(services): add service to process summary endpoint

--- src/Backend.Api.Core/Services/TitheService.cs
+++ src/Backend.Api.Core/Services/TitheService.cs
@@ -1,4 +1,7 @@
 using AutoMapper;
+using Backend.Api.Core.Common.HttpResults;
+using Backend.Api.Core.Common.HttpResults.Interfaces;
+using Backend.Api.Core.Dtos;
 using Backend.Api.Core.Entities;
 using Backend.Api.Core.Repositories;
 using Backend.Api.Core.Services.Base;
@@ -6,4 +9,23 @@ using Backend.Api.Core.Services.Base;
 namespace Backend.Api.Core.Services;
 
 public class TitheService(TitheRepository repository, IMapper mapper)
-    : ServiceBase<Tithe>(repository, mapper) { }
+    : ServiceBase<Tithe>(repository, mapper)
+{
+    private readonly TitheRepository _repository = repository;
+
+    public async Task<IOperationResult> GetSummaryAsync(
+        Guid congregationId,
+        int year,
+        CancellationToken ct
+    )
+    {
+        var aggregatedMonthlySummary = await _repository.GetAggregatedSummaryByMonth(
+            congregationId,
+            year,
+            ct
+        );
+
+        var summary = new TitheMonthlySummaryDto { Months = aggregatedMonthlySummary, Year = year };
+        return new SuccessResult<TitheMonthlySummaryDto>(summary);
+    }
+}
