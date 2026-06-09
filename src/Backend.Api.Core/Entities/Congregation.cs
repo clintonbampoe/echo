a6739a54f4b0b9a577045fe8d01fd18e021f0677
Clinton Bampoe <clintbamp@gmail.com>
2026-06-09 10:26:47 -0500

    feat(entities): set default value for Id in Congregation entity

--- src/Backend.Api.Core/Entities/Congregation.cs
+++ src/Backend.Api.Core/Entities/Congregation.cs
@@ -4,7 +4,7 @@ namespace Backend.Api.Core.Entities;
 
 public class Congregation : ISoftDeletableEntity, ISearchableEntity, IDateTrackedEntity
 {
-    public Guid CongregationId { get; set; }
+    public Guid CongregationId { get; set; } = Guid.CreateVersion7();
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     public string Name { get; set; } = string.Empty;
     public DateTime? DeletedAt { get; set; }
