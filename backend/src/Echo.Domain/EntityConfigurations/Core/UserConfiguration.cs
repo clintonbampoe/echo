652d4d8e93b03ba8964f000b447d31bdb2419337
Clinton Bampoe <clintbamp@gmail.com>
2026-08-20 22:03:24 +0000

    feat(api): add search by name endpoint for User entity
    - Add LastName, FirstName and Other fields to User entity and coalesce
    it into a single Name field
    - Add a trigram search index to that field
    - Expose endpoint to search for Users by Name

--- backend/src/Echo.Domain/EntityConfigurations/Core/UserConfiguration.cs
+++ backend/src/Echo.Domain/EntityConfigurations/Core/UserConfiguration.cs
@@ -9,8 +9,16 @@ public class UserConfiguration : PrimaryEntityConfigurationBase<User>
 {
     public override void ConfigureEntity(EntityTypeBuilder<User> builder)
     {
-        builder.HasIndex(u => u.EmailAddress)
-            .IsUnique()
-            .HasFilter("\"DeletedAt\" IS NULL");
+        builder.HasIndex(u => u.EmailAddress).IsUnique().HasFilter("\"DeletedAt\" IS NULL");
+
+        // Using immutable string concatenation (||) and COALESCE to handle nulls safely
+        builder
+            .Property(m => m.Name)
+            .HasComputedColumnSql(
+                $"TRIM(COALESCE(\"{nameof(User.LastName)}\", '') || ' ' || COALESCE(\"{nameof(User.FirstName)}\", '') || ' ' || COALESCE(\"{nameof(User.OtherNames)}\", ''))",
+                stored: true
+            );
+
+        builder.HasIndex(m => m.Name).HasMethod("GIN").HasOperators("gin_trgm_ops");
     }
 }
