3aab70afb1cfeeb4015e2cb5a54aa40bba764c3d
Muzammil327 <muzammilsafdarofficial@gmail.com>
2026-09-02 17:37:45 +0500

    fix(api): make computed Name columns non-writable (#110)

--- backend/src/Echo.Domain/Entities/Core/User.cs
+++ backend/src/Echo.Domain/Entities/Core/User.cs
@@ -17,7 +17,7 @@ public class User : IPrimaryEntity, ISearchableEntity
     public string FirstName { get; set; } = string.Empty;
     public string LastName { get; set; } = string.Empty;
     public string? OtherNames { get; set; } = string.Empty;
-    public string Name { get; set; } = string.Empty;
+    public string Name { get; private set; } = string.Empty;
 
     public DateTime CreatedAt { get; set; }
     public DateTime? DeletedAt { get; set; }
